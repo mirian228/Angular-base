bbe13848206ab6eebcdaf8f3132d42ce76b39692
mirian228 <pariksini@mail.ru>
2023-07-14 18:04:19 +0400

    Generating components and creating manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    HeaderComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule
